fd6827c611e3b40a3c82920e083c78d695514fbb
kanae nakaji <nae6p21@icloud.com>
2026-02-06 16:25:19 +0900

    feat: implement and customize authentication with Fortify

--- src/resources/views/items/index.blade.php
+++ src/resources/views/items/index.blade.php
@@ -8,17 +8,18 @@
 <div class="tab-wrapper">
     <div class="tab-switch">
         <!-- tab state -->
-        <input type="radio" id="tab-recommend" name="TAB" checked>
-        <input type="radio" id="tab-my-list" name="TAB">
+        <input type="radio" id="recommend" name="TAB" checked>
+        <input type="radio" id="my-list" name="TAB">
 
         <!-- tab header -->
         <div class="tab-header">
-            <label for="tab-recommend" class="tab">おすすめ</label>
-            <label for="tab-my-list" class="tab">マイリスト</label>
+            <a href="{{ route('/', ['tab' => 'tab1']) }}" class="tab">おすすめ</a>
+            <a href="{{ route('/?tab=mylist', ['tab' => 'tab2']) }}" class="tab">マイリスト</a>
         </div>
 
         <!-- tab contents -->
         <div class="tab-contents">
+            @if($activeTab === 'tab1')
             <section class="tab-content" id="content-recommend">
                 <div class="items">
                     <!-- foreachで画像と商品名を表示：おすすめ -->
@@ -26,24 +27,9 @@
                         <img src="" alt="商品画像" class="item__img">
                         <p class="item__name">商品名</p>
                     </div>
-                    <div class="item">
-                        <img src="" alt="商品画像" class="item__img">
-                        <p class="item__name">商品名</p>
-                    </div>
-                    <div class="item">
-                        <img src="" alt="商品画像" class="item__img">
-                        <p class="item__name">商品名</p>
-                    </div>
-                    <div class="item">
-                        <img src="" alt="商品画像" class="item__img">
-                        <p class="item__name">商品名</p>
-                    </div>
-                    <div class="item">
-                        <img src="" alt="商品画像" class="item__img">
-                        <p class="item__name">商品名</p>
-                    </div>
                 </div>
             </section>
+            @elseif($activeTab === 'tab2')
             <section class="tab-content" id="content-my-list">
                 <div class="items">
                     <!-- foreachで画像と商品名を表示：いいねのみ -->
@@ -53,6 +39,7 @@
                     </div>
                 </div>
             </section>
+            @endif
         </div>
     </div>
 </div>
