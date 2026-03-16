621d1d769f94c06fbaa953c24317bc27e117b784
kanae nakaji <nae6p21@icloud.com>
2026-03-16 21:35:46 +0900

    feat: add register-test

---
+++ src/tests/Feature/LogoutTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User;
+use Tests\TestCase;
+
+class LogoutTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * ログアウトできるか
+     */
+    public function test_user_can_logout(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/logout');
+
+        $response->assertRedirect(route('index'));
+
+        $response->assertGuest();
+
+    }
+}
