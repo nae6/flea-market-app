f9f9ef09a4688bdd7ccfbff29627487432cd47e1
kanae nakaji <nae6p21@icloud.com>
2026-03-22 20:27:44 +0900

    test: add favorite-test and comment-test

---
+++ src/database/factories/CommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Item;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'item_id' => Item::factory(),
+            'content' => fake()->sentence(),
+            'created_at' => now(),
+        ];
+    }
+}
